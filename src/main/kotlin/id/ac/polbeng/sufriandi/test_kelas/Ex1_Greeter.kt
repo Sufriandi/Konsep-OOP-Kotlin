4831ffa2a489e773e489c3fd4d343998c0987045
daintyest <Sufriandi446@gmail.com>
2025-10-18 00:44:25 +0700

    Initial commit

--- src/main/kotlin/id/ac/polbeng/sufriandi/test_kelas/Ex1_Greeter.kt
+++ src/main/kotlin/id/ac/polbeng/sufriandi/test_kelas/Ex1_Greeter.kt
@@ -18,3 +18,12 @@ class Ex1_Greeter {
         return "$text $name"
     }
 }
+fun main() {
+    val greeter = Ex1_Greeter()
+    greeter.greet()
+    greeter.text = "Hi"
+    greeter.greet("Anton")
+    greeter.greet("Budi")
+    greeter.text = "Hello programmer"
+    println(greeter.with_ret_val("Dono"))
+}
