a7c8758e2f13e26d6260e16ee1a610b49480f6fe
daintyest <Sufriandi446@gmail.com>
2025-10-17 23:37:53 +0700

    Initial commit

---
+++ src/main/kotlin/id/ac/polbeng/sufriandi/test_kelas/Ex35_DataClassD.kt
@@ -0,0 +1,6 @@
+package org.example.id.ac.polbeng.sufriandi.test_kelas
+
+fun main() {
+    val meri = Student("Meri", 20)
+    println(meri.toString())
+}
---
+++ src/main/kotlin/id/ac/polbeng/sufriandi/test_kelas/Ex36_DataClassE.kt
@@ -0,0 +1,8 @@
+package org.example.id.ac.polbeng.sufriandi.test_kelas
+
+fun main() {
+    val meri = Student("Meri", 20)
+    val(name, age) = meri
+    println("Name = $name")
+    println("Age = $age")
+}
